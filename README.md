399e00a656550f1382b0a2c504008ad516a95e20
Andy Dobson <andy.dobson@publicispoke.com>
2022-12-21 16:54:24 +0000

    move it to alphabox

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Digitonal.com 2022
 
-
-
 ##
+
 11ty
 tailwind
 
@@ -10,3 +9,4 @@ Using this guide:
 
 https://css-tricks.com/eleventy-starter-with-tailwind-css-alpine-js/
 
+https://tylerduprey-52451.medium.com/a-perfect-square-with-css-964499440998
---
+++ styles/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["_site/**/*.html"],
+  safelist: [],
+  theme: {
+    extend: {
+      colors: {
+        change: "transparent",
+      },
+      padding: {
+        sq: "100%",
+      },
+    },
+  },
+  plugins: [],
+};
